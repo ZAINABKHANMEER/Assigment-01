09242425613ae9e5367a7530dc272d670eb6309b
ZAINABKHANMEER <gulalaimalalai1998@gmail.com>
2024-11-07 11:58:20 -0800

    Add files via upload

---
+++ Assigment.py
@@ -0,0 +1,21 @@
+num1=100
+num2=324
+num3=478
+#calculate sum
+sum=num1+num2+num3
+print("sum:",sum)
+#calculate diffrence
+diffrence=num1-num2-num3
+print("difference:,difference")
+#calculate product
+product=num1*num2*num3
+print("product",product)
+#calculate average
+average=(num1+num2+num3)/3
+print("average",average)
+#area of triangle
+breath=6
+height=7
+area=0.5*breath*height
+print("triangle area",area)
+
